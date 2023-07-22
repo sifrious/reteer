eee29e5e116a814c1a4a9176215bacccea28d52f
Mary <mmebit@icloud.com>
2023-07-22 01:20:06 -0500

    no public bool :/

--- database/migrations/2023_07_22_050452_create_tasks_table.php
+++ database/migrations/2023_07_22_050452_create_tasks_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('is_public');
             $table->timestamps();
         });
     }
