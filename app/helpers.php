4e0391ea75c74a7c9501d995060e21eb0e7e5d3e
Mary <mmebit@icloud.com>
2023-07-25 18:25:17 -0400

    cleaned helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -1,10 +1,1 @@
 <?php
-
-use Illuminate\Support\Str;
-use Filament\Notifications\Collection;
-use Illuminate\Support\Facades\Cache;
-
-function clean_header(string $item): string {
-    return str($item)->lower()->replace(["(", ")", "*"], '')->replace([" ", "\n", "__"], "_");
-};
-?>
