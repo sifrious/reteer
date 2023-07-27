0cd25c1d2ba96c447e1e494eebb1688c2e758332
mmelines <64270-mmelines@users.noreply.drupalcode.org>
2023-07-27 19:38:59 -0400

    display tasks in tasks.index.blade view

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(Request $request)
     {
-        return view('tasks.index');
+        $tasks = Task::all();
+        return view('tasks.index', [
+            'tasks' => $tasks
+        ]);
     }
 }
