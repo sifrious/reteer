896117d6a4181a7b1bf8bbdc7182492510efdd7f
Filip Ganyicz <ganyicz.filip@gmail.com>
2023-07-29 01:01:59 +0200

    Protect /tasks route

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth')->group(function () {
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
 
 Route::redirect('/', '/tasks');
 
