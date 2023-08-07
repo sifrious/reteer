6dcd73f13c677e412a2559b72dad0baf5aac9961
Mary Perry <83845130+mmelines@users.noreply.github.com>
2023-08-07 14:54:54 -0400

    test comment

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,6 +37,7 @@ class User extends Authenticatable implements FilamentUser
 
     /**
      * The attributes that should be cast.
+     * This is a comment!
      *
      * @var array<string, string>
      */
