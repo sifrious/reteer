999ba27eca64bbd8b3f450cecf921218cbd9d535
Mary Perry <83845130+mmelines@users.noreply.github.com>
2023-08-13 06:08:23 -0400

    added Task Subtypes

---
+++ database/migrations/2023_08_13_100718_create_task_subtypes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task_subtypes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('task_type_id')->constrained();
+            $table->char('name', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task_subtypes');
+    }
+};
