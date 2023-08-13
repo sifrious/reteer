b2d210b945d0d3d758b24594467ab151e1b88090
Mary Perry <83845130+mmelines@users.noreply.github.com>
2023-08-13 06:01:26 -0400

    added ClientLocations model

---
+++ database/migrations/2023_08_13_100040_create_client_locations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('client_locations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained();
+            $table->foreignId('location_id')->constrained();
+            $table->boolean('is_active')->nullable();
+            $table->date('move_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('client_locations');
+    }
+};
