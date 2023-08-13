11228b97c181a3ce8f8dd1e97d9b8f1502970389
Mary Perry <83845130+mmelines@users.noreply.github.com>
2023-08-13 10:40:09 -0400

    updated tasks table

--- database/migrations/2023_07_22_050452_create_tasks_table.php
+++ database/migrations/2023_07_22_050452_create_tasks_table.php
@@ -13,10 +13,21 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('sheets_id');
+            $table->string('sheets_row');
+            $table->timestamp('sheets_created_at');
+            $table->timestamp('updated_at');
             $table->string('name');
+            $table->string('originator');
             $table->string('start_date')->nullable();
             $table->string('start_time')->nullable();
             $table->integer('public');
+            $table->string('client_address')->nullable();
+            $table->string('task_description');
+            $table->string('destination')->nullable();
+            $table->string('volunteer')->nullable();
+            $table->string('status')->nullable();
+            $table->string('contact_information')->nullable();
             $table->timestamps();
         });
     }
