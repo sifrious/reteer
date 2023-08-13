42e883b07d25e1e3cee41714626434d7fdfc4092
Mary Perry <83845130+mmelines@users.noreply.github.com>
2023-08-13 06:06:51 -0400

    added Task Types

---
+++ app/Http/Controllers/TaskTypesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TaskTypes;
+use Illuminate\Http\Request;
+
+class TaskTypesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TaskTypes $taskTypes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TaskTypes $taskTypes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TaskTypes $taskTypes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TaskTypes $taskTypes)
+    {
+        //
+    }
+}
