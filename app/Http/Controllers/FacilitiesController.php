b44ecd5bf4b87b56d8965b7219ac239f758c966c
Mary Perry <83845130+mmelines@users.noreply.github.com>
2023-08-13 06:02:59 -0400

    added Facilities table

---
+++ app/Http/Controllers/FacilitiesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Facilities;
+use Illuminate\Http\Request;
+
+class FacilitiesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Facilities $facilities)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Facilities $facilities)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Facilities $facilities)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Facilities $facilities)
+    {
+        //
+    }
+}
