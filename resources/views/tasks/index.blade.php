96b3e1f961da8fa591cde6646eccb31563fb0b70
Filip Ganyicz <ganyicz.filip@gmail.com>
2023-08-18 00:55:02 +0200

    Remove dark mode

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex items-center justify-between">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Tasks') }}
             </h2>
             <button class="text-white bg-blue-500 px-6 py-2 rounded font-bold">
@@ -15,18 +15,18 @@
     <div class="py-12">
         <div class="mb-10 max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div
-                class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 text-gray-900 dark:text-gray-100">
+                class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 text-gray-900">
                 {{ __("There are {$count} Unassigned Tasks") }}
             </div>
         </div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-2">
-            <div class="text-gray-900 dark:text-gray-100 text-2xl">
+            <div class="text-gray-900 text-2xl">
                 {{ __('Upcoming Unassigned Tasks') }}
             </div>
             @foreach ($tasks as $task)
                 <a href="{{ url('tasks/') . '/' . $task->id }}">
-                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900">
                             <div>
                                 @if ($task->start_date != '')
                                     <span>{{ $task->start_date }}</span>
