2ff66540643955f2cacd6d8b9f0bf57aaf4408d9
Mary Perry <83845130+mmelines@users.noreply.github.com>
2023-09-03 13:40:48 -0400

    added Task model

---
+++ reteer-app/database/migrations/2023_09_03_173916_create_tasks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('sheets_id')->unique();
+            $table->string('sheets_row');
+            $table->string('sheets_created_at')->nullable();
+            $table->string('name')->nullable();
+            $table->string('author')->nullable();
+            $table->string('start_date')->nullable();
+            $table->string('start_time')->nullable();
+            $table->integer('public');
+            $table->string('client_address')->nullable();
+            $table->string('task_description');
+            $table->string('destination')->nullable();
+            $table->string('volunteer')->nullable();
+            $table->string('status')->nullable();
+            $table->string('contact_information')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
