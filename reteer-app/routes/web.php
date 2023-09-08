6d62dab25f3b8f6c60d02b1b522d106defd061f5
Mary Perry <83845130+mmelines@users.noreply.github.com>
2023-09-08 01:29:24 -0400

    added routes from previous version

--- reteer-app/routes/web.php
+++ reteer-app/routes/web.php
@@ -22,7 +22,69 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+    //user dashboard
     Route::get('/dashboard', function () {
         return view('dashboard');
-    })->name('dashboard');
+    })->name('tasks.board');
+    // all tasks
+    Route::get(
+        '/tasks/user/{user}',
+        [TaskController::class, 'display']
+    )->name('tasks.display');
+    Route::get(
+        '/tasks',
+        [TaskController::class, 'index']
+    )->name('tasks.index');
+    // view create form
+    Route::get(
+        '/tasks/new',
+        [TaskController::class, 'create']
+    )->name('tasks.create');
+    Route::post(
+        '/tasks/new',
+        [TaskController::class, 'store']
+    )->name('tasks.store');
+    // confirm task has been posted from spreadsheet
+    Route::get(
+        '/tasks/{task}/new',
+        [TaskController::class, 'confirmCreate']
+    )->name('tasks.confirmCreate');
+    Route::post(
+        '/tasks/new',
+        [TaskController::class, 'store']
+    )->name('tasks.store');
+    Route::post(
+        '/tasks/new/{task}/confirm',
+        [TaskController::class, 'confirm']
+    )->name('tasks.confirmStore');
+    Route::get(
+        '/tasks/{task}',
+        [TaskController::class, 'show']
+    )->name('tasks.show');
+    // view edit form
+    Route::get(
+        '/tasks/{task}/edit',
+        [TaskController::class, 'edit']
+    )->name('tasks.edit');
+    Route::post(
+        '/tasks/{task}/edit',
+        [TaskController::class, 'update']
+    )->name('tasks.update');
+    // update existing task
+    Route::get(
+        '/tasks/{task}/update/',
+        [TaskController::class, 'update']
+    )->name('tasks.update');
+    Route::get(
+        '/profile',
+        [ProfileController::class, 'edit']
+    )->name('profile.edit');
+    Route::patch(
+        '/profile',
+        [ProfileController::class, 'update']
+    )->name('profile.update');
+    Route::delete(
+        '/profile',
+        [ProfileController::class, 'destroy']
+    )->name('profile.destroy');
 });
